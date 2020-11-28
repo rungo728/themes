839fbd0584a94e1363fd51d1e36f6bb912ee7926
rungo728 <53888314+rungo728@users.noreply.github.com>
2020-11-28 15:50:34 +0900

    create category.php
    カテゴリー
    タグ
    日付ごとに
    関連記事が表示するための記述

--- tf30/date.php
+++ tf30/date.php
@@ -14,7 +14,7 @@
         </div><!-- /breadcrumb -->
 
         <div class="archive-head m_description">
-        <div class="archive-lead">TAG</div>
+        <div class="archive-lead">DATE</div>
         <h1 class="archive-title m_category"><?php the_archive_title(); //一覧ページ名を表示 ?></h1><!-- /archive-title -->
         <div class="archive-description">
         <p><?php the_archive_description(); //説明を表示 ?></p>
