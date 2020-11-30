5a8a921ff368994c9c63ae70f80deff49566db66
rungo728 <53888314+rungo728@users.noreply.github.com>
2020-11-30 14:20:37 +0900

    get template.php
    ページネーションの記述が一緒なのでテンプレ化しました。

--- tf30/tag.php
+++ tf30/tag.php
@@ -72,23 +72,7 @@
 				</div><!-- /entries -->
         <?php endif; ?>
 
-        <?php if (paginate_links() ) : //ページが1ページ以上あれば以下を表示 ?>
-          <!-- pagenation -->
-          <div class="pagenation">
-          <?php
-          echo
-          paginate_links(
-          array(
-          'end_size' => 1,
-          'mid_size' => 1,
-          'prev_next' => true,
-          'prev_text' => '<i class="fas fa-angle-left"></i>',
-          'next_text' => '<i class="fas fa-angle-right"></i>',
-          )
-          );
-          ?>
-          </div><!-- /pagenation -->
-          <?php endif; ?>
+        <?php get_template_part('template-parts/pagenation'); ?>
           
 			</main><!-- /primary -->
 
