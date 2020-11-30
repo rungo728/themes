a77c48baf5354d6c4b69ed5af9e3adf7c9644f8c
rungo728 <53888314+rungo728@users.noreply.github.com>
2020-11-30 11:13:55 +0900

    Update functions.php
    snsシェアボタンを反映させるための記述

--- tf30/functions.php
+++ tf30/functions.php
@@ -37,6 +37,12 @@ function my_script_init()
 wp_enqueue_style('fontawesome', 'https://use.fontawesome.com/releases/v5.8.2/css/all.css', array(), '5.8.2', 'all');
 wp_enqueue_style('my', get_template_directory_uri() . '/css/style.css', array(), '1.0.0', 'all');
 wp_enqueue_script('my', get_template_directory_uri() . '/js/script.js', array( 'jquery' ), '1.0.0', true);
+
+//sns.jsを追記
+if( is_single() ){
+ wp_enqueue_script('sns', get_template_directory_uri() . '/js/sns.js', array( 'jquery' ), '1.0.0', true);
+}
+    
 }
 add_action('wp_enqueue_scripts', 'my_script_init');
 
